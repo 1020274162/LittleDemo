3c61d6999936862d1eb525acb168f25b5c4d9940
1020274162 <88483719+1020274162@users.noreply.github.com>
2022-06-15 02:45:29 +1000

    LittleDemo

---
+++ Editor/RjNumSet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RjNumSet : MonoBehaviour
+{
+    public static int NumT;
+    [Header("递增数字")]
+    public int Num;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        NumT=Num;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
